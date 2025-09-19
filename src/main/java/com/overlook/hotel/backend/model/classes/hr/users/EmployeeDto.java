3708e72c6de00d862b4ca3f806230ebd2b7a2f3f
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-19 09:02:55 +0200

    WIP: preparing to pull Database

--- src/main/java/com/overlook/hotel/backend/model/classes/hr/users/EmployeeDto.java
+++ src/main/java/com/overlook/hotel/backend/model/classes/hr/users/EmployeeDto.java
@@ -5,7 +5,6 @@ import com.overlook.hotel.backend.model.classes.logistic.RoomDto;
 import com.overlook.hotel.backend.model.classes.hr.LeaveRequestDto;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import java.util.Date;
