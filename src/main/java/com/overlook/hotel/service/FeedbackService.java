a1d05df46cb7754de42907ff0a16d5ba2372b5b7
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-09-25 13:12:33 +0200

    added Feedback Controller and Feedback Service

---
+++ src/main/java/com/overlook/hotel/service/FeedbackService.java
@@ -0,0 +1,43 @@
+package com.overlook.hotel.service;
+
+import com.overlook.hotel.database.Entity.Feedback;
+import com.overlook.hotel.repository.FeedbackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+    private final FeedbackRepository feedbackRepository;
+
+    // CRUD: READ (Retrieve all feedbacks from the database)
+    public List<Feedback> getAllFeedbacks() {
+        return feedbackRepository.findAll();
+    }
+
+    // CRUD: READ (Retrieve a feedback by its ID)
+    public Feedback getFeedbackById(Long id) {
+        return feedbackRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Feedback not found"));
+    }
+
+    // CRUD: CREATE (Save a new feedback to the database)
+    public Feedback createFeedback(Feedback feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    // CRUD: UPDATE (Update an existing feedback in the database)
+    public Feedback updateFeedback(Long id, Feedback details) {
+        Feedback feedback = getFeedbackById(id);
+        feedback.setComment(details.getComment());
+        feedback.setRating(details.getRating());
+        feedback.setClient(details.getClient());
+        return feedbackRepository.save(feedback);
+    }
+
+    // CRUD: DELETE (Remove a feedback from the database by its ID)
+    public void deleteFeedback(Long id) {
+        feedbackRepository.deleteById(id);
+    }
+}
