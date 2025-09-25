0766a70d49a978ac097ac2c6987d3ed75cd3c5a3
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-25 09:15:52 +0200

    preparing to merge with preprod

--- src/main/java/com/overlook/hotel/service/frontDtoService/DtoToFront.java
+++ src/main/java/com/overlook/hotel/service/frontDtoService/DtoToFront.java
@@ -1,4 +1,4 @@
-package com.overlook.hotel.service.frontService;
+package com.overlook.hotel.service.frontDtoService;
 
 
 /**
--- src/main/java/com/overlook/hotel/service/frontDtoService/FrontToDto.java
+++ src/main/java/com/overlook/hotel/service/frontDtoService/FrontToDto.java
@@ -1,7 +1,9 @@
-package com.overlook.hotel.service.frontService;
+package com.overlook.hotel.service.frontDtoService;
 
 /**
  * placeholder for front to Dto communications
  */
 public class FrontToDto {
+
+
 }
