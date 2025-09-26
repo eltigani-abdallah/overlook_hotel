a975ad1fa91735b11f83e0b665a210c31534e7dc
olivier-portal <olivierportal09@gmail.com>
2025-09-26 14:32:06 +0200

    feat: hash password

--- src/main/java/com/overlook/hotel/SecurityConfig.java
+++ src/main/java/com/overlook/hotel/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
@@ -26,5 +28,10 @@ public class SecurityConfig extends VaadinWebSecurity {
         return web -> web.ignoring().requestMatchers(
                 "/images/**"
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
