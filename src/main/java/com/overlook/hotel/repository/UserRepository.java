efabc793118cdd5781bab86b34fd2f64776ec566
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-27 17:11:51 +0200

    refactor: deleted UserRepository due to overlap with ClientRepositoryÃ

--- src/main/java/com/overlook/hotel/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.overlook.hotel.repository;
-
-import com.overlook.hotel.Entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Client, Long> {}
